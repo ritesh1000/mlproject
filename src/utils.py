5064cf55650a9c18be0e4f4dd5fb32ac9d00d435
Ritesh Natekar <ritesh1000.n@gmail.com>
2026-05-15 16:30:07 +0530

    Committing changes in Model trainer with Hyper parameter tunnnig

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,13 +18,20 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_model(x_train, y_train, x_test, y_test, models):
+def evaluate_model(x_train, y_train, x_test, y_test, models,params):
     try:
         report={}
         for i in range(len(list(models))):
             model= list(models.values())[i]
+            param=params[list(models.keys())[i]]
 
-            model.fit(x_train, y_train)
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+       
+            #model.fit(x_train, y_train)
 
             y_train_pred= model.predict(x_train)
             y_test_pred= model.predict(x_test)
